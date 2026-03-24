63b1de1720f02aeacc1aebf178df4bdba1722816
I1obody <90924529+I1obody@users.noreply.github.com>
2026-03-24 22:53:23 +0300

    Add configuration settings for bot and venue

---
+++ config.py
@@ -0,0 +1,18 @@
+token= "bot_token_here"
+admin_id = id of ur admin
+dev_id = dev id
+owner_id = id of cheff
+# Наша геолокация
+venue_lat = 00.000000 # Широта заведения
+venue_lon = 00.000000 # Долгота заведения
+venue_title = "NAME HERE"
+venue_address = "ADRESS HERE"
+# Часы работы
+ADMIN_WORKING_HOURS = {
+    'mon_fri_start': 10, # hour in 24h
+    'mon_fri_end': 22,
+    'sat_sun_start': 10,
+    'sat_sun_end': 16,
+    'timezone': 'Europe/Moscow'
+}
+REQUEST_QUEUE_FILE = 'db/request_queue.json'
